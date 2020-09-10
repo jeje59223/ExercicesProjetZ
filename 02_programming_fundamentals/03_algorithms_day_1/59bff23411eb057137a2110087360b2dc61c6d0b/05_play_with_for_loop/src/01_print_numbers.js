d967555159fa157458990b24b76173b6ed7fe7f4
jeje59223 <jcnockaert@hotmail.fr>
2020-09-10 18:14:17 +0200

    exercices

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/01_print_numbers.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/01_print_numbers.js
@@ -1,6 +1,6 @@
 // Write on `stdout` each number from 0 to 100 with a for loop
-let stdout = "";
+
 for(let i = 0; i <= 100; i++){
-  stdout += i + "\n";
+  let stdout = i ;
+  console.log(stdout);
 }
-console.log(stdout);
--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/02_digits_again.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/02_digits_again.js
@@ -1,9 +1,3 @@
 // ## Build an array
 //
 // Create the famous `digits` array using a `for-loop`.
-//  const digits = [];
-
-//  for(let i = 0; i < 10; i++){
-//       digits.push(i);
-// }
-// console.log(digits);
