559094e57969115344419888b1944dd259021807
jeje59223 <jcnockaert@hotmail.fr>
2020-09-10 17:00:55 +0200

    exercice 4 validé exoplanete

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/04_play_with_conditional_statement_exoplanets/src/index.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/04_play_with_conditional_statement_exoplanets/src/index.js
@@ -3,7 +3,31 @@ const star = require("./data/starData");
 const planet = require("./data/exoplanetData");
 
 // Your code here:
-let canHabitateLife;
+let canHabitateLife = true;
+
+if(star.spectralClass === "O" || star.spectralClass === "B" || star.spectralClass === "A" || star.spectralClass === "F" || star.spectralClass === "G"){
+    canHabitateLife = !canHabitateLife;
+}
+
+if(!planet.habitalZone){
+    canHabitateLife = !canHabitateLife;
+}
+
+if(!planet.rotationStability){
+    canHabitateLife = !canHabitateLife;
+}
+
+if(planet.radius <= 0.5){
+    canHabitateLife = !canHabitateLife;
+}
+
+if(planet.radius >= 2.5){
+    canHabitateLife = !canHabitateLife;
+}
+
+if(planet.mass < 0.5 && planet.radius < 1){
+    canHabitateLife = !canHabitateLife;
+}
 
 // Don't modifiy this line for test to work properly.
 module.exports = canHabitateLife;
