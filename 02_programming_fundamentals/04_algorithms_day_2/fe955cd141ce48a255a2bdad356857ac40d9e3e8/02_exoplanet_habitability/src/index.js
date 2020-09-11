165fdea912b31706b4fba87ede3cd31b5a762824
jeje59223 <jcnockaert@hotmail.fr>
2020-09-11 12:45:24 +0200

    exercice exoplanet validé

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/02_exoplanet_habitability/src/index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/02_exoplanet_habitability/src/index.js
@@ -9,23 +9,27 @@ const planet = {
 
 function canHabitateLife(star, planet) {
   // Code the function here.
-//   if(star.spectralClass !== "K" & star.spectralClass !== "M"){
-//     return false;
-// }
+  if((star.spectralClass !== "K" & star.spectralClass !== "M") || !planet.habitalZone || !planet.rotationStability || (planet.radius <= 0.5 || planet.radius >= 2.5) || (planet.mass < 0.5 && planet.radius < 1)){
+    return false;
+  }
+  return true;
+}
 
 // Do not remove last lines, it is for tests
 module.exports = canHabitateLife;
 
 // should not return 'undefined' (2 ms)
-// ✕ should return a Boolean (3 ms)
-// ✕ should return false for stars with spectral class O, B, A, F, G.  (1 ms)
-// ✕ should return true for suitable planets and stars with spectral class K, M.
+// should return a Boolean (3 ms)
+// should return false for stars with spectral class O, B, A, F, G.  (1 ms) 
+// should return false a planet outside an habitable zone 
+// should return false for a planet without a stable rotation (1 ms)
+// should return false for a planet with a radius <= 0.5
+// should return false for a planet with a radius >= 2.5
+// should return false for a planet with a mass >= 3 (1 ms)
+// should return false for a planet with a mass < 0.5 and a radius < 1
+
+
+// should return true for suitable planets and stars with spectral class K, M.
 // Planets' criteria with star of spectral classes K and M
-// ✕ should return true for an Earth like planet (1 ms)
-// ✕ should return false a planet outside an habitable zone
-// ✕ should return false for a planet without a stable rotation (1 ms)
-// ✕ should return false for a planet with a radius <= 0.5
-// ✕ should return false for a planet with a radius >= 2.5
-// ✕ should return false for a planet with a mass >= 3 (1 ms)
-// ✕ should return false for a planet with a mass < 0.5 and a radius < 1
-// ✕ should return true for a planet with a mass < 0.5 and a radius > 1 (1 ms)
+// should return true for an Earth like planet (1 ms)
+// should return true for a planet with a mass < 0.5 and a radius > 1 (1 ms)
