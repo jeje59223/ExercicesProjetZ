74dfa2ea5ae35c03513eb1fd42e203bea7a810d5
jeje59223 <jcnockaert@hotmail.fr>
2020-09-12 22:14:43 +0200

    exercice 07 pattern bonus validé

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/07_pattern_bonus/src/01_complete_the_pattern.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/07_pattern_bonus/src/01_complete_the_pattern.js
@@ -1,3 +1,12 @@
 // Modify this value to test with other values
 const numberOfLine = 5;
 // Your code here ⬇
+let numbers = "";
+
+for(let i = numberOfLine; i > 0; i--){
+  numbers += i;
+}
+
+for(let i = numberOfLine; i > 0; i --){
+  console.log(numbers.slice(0, i));
+}
