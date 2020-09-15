7932ebb730b0d1d1ff852b737e690e8ec641d445
jeje59223 <jcnockaert@hotmail.fr>
2020-09-15 12:38:06 +0200

    exercice 2 first project

--- 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/02_number_game/src/numberGame.js
+++ 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/02_number_game/src/numberGame.js
@@ -1,5 +1,31 @@
+
+
 function numberGame(reader, min = 1, max = 100) {
   // code here
+  const numberSecret = Math.round(Math.random() * (max - min) + min);
+  console.log(numberSecret);
+  let question = "Enter a number : \n";
+  const startGame = () => {
+    reader.question(question, (number) => {
+      if(isNaN(number)){
+        question = `${number} is not a number ! \n`;
+        startGame();
+      } else if(number < min || number > max){
+        question = `The number is between ${min} and ${max} \n`;
+        startGame();
+      } else if(number < numberSecret){
+        question = "too low \n";
+        startGame();
+      } else if( number > numberSecret){
+        question = "too high \n";
+        startGame();
+      } else {
+        console.log("You won! \n");
+        reader.close();
+      }
+    });
+  };
+  startGame();
 }
 
 module.exports = numberGame;
