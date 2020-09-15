ac5e221703d550423192cfe231b40c7591f05ad2
jeje59223 <jcnockaert@hotmail.fr>
2020-09-15 10:19:00 +0200

    exo 1 first project

---
+++ 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/01_identity_form/src/identityForm.js
@@ -0,0 +1,13 @@
+const identityForm = (reader) => {
+  // Code here
+  reader.question("first name\n >", (firstname) => {
+    reader.question("last name\n >", (name) => {
+      reader.question("age\n", (age) => {
+        console.log(`Your name is ${firstname} ${name} and you are ${age}.`);
+      });
+    });
+  });
+};
+
+// Leave line below for tests to work
+module.exports = identityForm;
