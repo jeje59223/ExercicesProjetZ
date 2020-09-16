a3a0486ec070cc9faa284297961bd59c636b8714
jeje59223 <jcnockaert@hotmail.fr>
2020-09-16 10:17:54 +0200

    exercice 1 : This, validé

---
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/01_using_this/src/person.js
@@ -0,0 +1,14 @@
+const person = {
+  firstname: "Abdel",
+  lastname: "Sadki",
+  age: 42,
+  fullname: function () {
+    return `${this.firstname} ${this.lastname}`;
+  },
+  // Complete here
+  introduceMyself: function () {
+    return `Hello! I'm ${this.fullname()} and I'm ${this.age}.`;
+  }
+};
+
+module.exports = person;
