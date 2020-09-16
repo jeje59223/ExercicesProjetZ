a267c63871c8ad30c5032492846c9d03d35e58a9
jeje59223 <jcnockaert@hotmail.fr>
2020-09-16 14:13:08 +0200

    exercice 2 this validé

--- 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/car.js
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/car.js
@@ -1,5 +1,31 @@
 const car = {
   // Complete here
+  speed: 0,
+  tps: 0,
+  distance: 0,
+
+  start: function () {
+    this.speed = 0;
+    this.tps = 0;
+    this.distance = 0;
+    return this;
+  },
+  
+  changeSpeed: function (speed) {
+    this.speed = speed;
+    return this;
+  },
+
+  drive: function (tps) {
+    this.tps = tps;
+    this.distance += this.speed * this.tps / 60;
+    return this;
+  },
+
+  showDistance: function () {
+    console.log(this.distance + " Km");
+    return this;
+  },
 };
 
 module.exports = car;
--- 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/index.js
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/index.js
@@ -1,3 +1,6 @@
 const car = require("./car");
 
 car.start().changeSpeed(130).drive(42).showDistance();
+car.start().changeSpeed(100).drive(30).changeSpeed(50).drive(30).showDistance(); // => "75 Km" 
+car.start().changeSpeed(130).showDistance();
+car.start().changeSpeed(100).drive(30).showDistance();
