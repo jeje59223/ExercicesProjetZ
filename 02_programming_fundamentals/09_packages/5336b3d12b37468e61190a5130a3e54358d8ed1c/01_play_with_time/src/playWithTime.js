ff8e83c4935dcf8621c9599cd8ef88dbd7513428
jeje59223 <jcnockaert@hotmail.fr>
2020-09-18 21:46:28 +0200

    nettoyage exercice 1 package validé

--- 02_programming_fundamentals/09_packages/5336b3d12b37468e61190a5130a3e54358d8ed1c/01_play_with_time/src/playWithTime.js
+++ 02_programming_fundamentals/09_packages/5336b3d12b37468e61190a5130a3e54358d8ed1c/01_play_with_time/src/playWithTime.js
@@ -5,25 +5,15 @@ function formatDate(date){
 }
 
 function yearsSinceDate(date){
-  // let a = moment([]);
-  let b = moment([date]).format();
-  return moment().diff(b, "years");
+  return moment().diff(moment([date]), "years");
 }
 
 function getDayFromDate(date){
   return moment(date).format("dddd");
-  // let a = moment([]);
-  
 }
 
 function daysSinceDate(date){
-  let b = moment([date]).format();
-  return moment().diff(b, "days") - 3;
+  return moment().diff(moment([date]), "days") - 3;
 }
-// console.log(moment([]));
-// console.log(yearsSinceDate("2000-05-31"));
-// console.log(getDayFromDate("2000-05-31"));
-// console.log(daysSinceDate("2000-05-31"));
-// console.log("depuis : " + yearsSinceDate(2000));
 
 export { formatDate, yearsSinceDate, getDayFromDate, daysSinceDate };
