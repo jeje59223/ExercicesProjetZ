64077381d99c75ace339a7a43c31a133af00236b
jeje59223 <jcnockaert@hotmail.fr>
2020-09-23 14:13:00 +0200

    exercice 3 validé TypeScript

--- 03_backend_developement/01_typescript_101/1bc25b477fadbef8f726030942699167117a51ef/03_typed_class/src/index.ts
+++ 03_backend_developement/01_typescript_101/1bc25b477fadbef8f726030942699167117a51ef/03_typed_class/src/index.ts
@@ -1,5 +1,29 @@
 class Bird {
   // Write your code here.
+  age: number;
+
+  constructor(age: number){
+    this.age = age;
+  }
+
+  sing(): void {
+    console.log("Cui cui");
+  }
+
+  fly(second: number):void {
+    if(this.age <= 1){
+      console.log("The bird is too young to fly");
+    }
+    if(this.age > 1 && this.age <= 3){
+      const distance: number = 1 * second;
+      console.log(`The bird flew ${distance}m meters in ${second} seconds.`);
+    }
+    if(this.age > 3){
+      const distance: number = 2 * second;
+      console.log(`The bird flew ${distance}m meters in ${second} seconds.`);
+      
+    }
+  }
 }
 
 // Leave the line below for tests to work properly.
