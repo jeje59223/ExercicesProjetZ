6cfb9f66de7133acc5b5bdfb37fb8cdbcd92047f
jeje59223 <jcnockaert@hotmail.fr>
2020-09-26 14:19:36 +0200

    exercice fizzBuzz avec map() validé

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/06_fizz_buzz_with_map/src/index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/06_fizz_buzz_with_map/src/index.js
@@ -1,6 +1,15 @@
-function fizzBuzz(list) {
-  // Code your function here.
+function fizzBuzz(list){
+  let newTab = [];
+
+  newTab = list.map (i => 
+    (i % 3 === 0 && i % 5 === 0) ? "FizzBuzz" : 
+      (i % 3 === 0) ? "Fizz" :
+        (i % 5 === 0) ? "Buzz" : i);
+  return newTab;
 }
+
+const tab = [1,2,3,4,5,6,7,8,9,15,150,3];
+console.log(fizzBuzz(tab));
 
 // Leave the line below for tests to work properly.
 module.exports = fizzBuzz;
