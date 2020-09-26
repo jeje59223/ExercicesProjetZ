b778235d3afce398abfe6969eaec382e2ef4cb81
jeje59223 <jcnockaert@hotmail.fr>
2020-09-26 15:49:42 +0200

    exercice fonctions : 07_join_array

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/07_join_array/src/index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/07_join_array/src/index.js
@@ -1,7 +1,12 @@
-function joinArray() {
+function joinArray(tab, separator) {
   // Code your function here.
+  return tab.reduce(function(accumulator, currentValue){
+    return accumulator + separator + currentValue;
+  });
   
 }
+const tab = ["zero", "one", "two", "four", "five", "six"];
+console.log(joinArray(tab, " - "));
 
 // ⚠ Do not remove me ! It's for tests
 module.exports = joinArray;
