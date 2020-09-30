141ea3743020bd5261902d386815e167f649831f
jeje59223 <jcnockaert@hotmail.fr>
2020-09-30 08:32:56 +0200

    exercice Promise

--- 03_backend_developement/05_promises/d3df63ab202e290c26694e701b5122b8cd65073d/02_chuck_fetch/src/chuck.ts
+++ 03_backend_developement/05_promises/d3df63ab202e290c26694e701b5122b8cd65073d/02_chuck_fetch/src/chuck.ts
@@ -1,12 +1,11 @@
-import fetch, { Response } from "node-fetch";
-
-function getCategories() {
-  // Your code goes here
+import fetch from "node-fetch";
+function getCategories(): Promise<string[]> {
+  return fetch("https://api.chucknorris.io/jokes/categories").then((Response) => Response.json());
 }
-
-function getChuckNorrisJoke() {
-  // Your code goes here
+function getChuckNorrisJoke(selectedCategory: string): Promise<string> {
+  return fetch(`https://api.chucknorris.io/jokes/random?category=${selectedCategory}`).then((Response) =>
+    Response.json().then((joke) => joke.value)
+  );
 }
-
 // Leave the line below for tests to work properly
 export { getCategories, getChuckNorrisJoke };
--- 03_backend_developement/05_promises/d3df63ab202e290c26694e701b5122b8cd65073d/03_github_client/src/github-client.ts
+++ 03_backend_developement/05_promises/d3df63ab202e290c26694e701b5122b8cd65073d/03_github_client/src/github-client.ts
@@ -1,30 +1,47 @@
-import fetch, { Response } from "node-fetch";
+import fetch, { FetchError, Response } from "node-fetch";
 
 export type Repo = {
   url: string;
   name: string;
-  [key: string]: string |number |boolean |null;
-}
+  [key: string]: string | number | boolean | null;
+};
 
 export class GithubClient {
-  static getReposUrl() {
-    // You code goes here
+  static getReposUrl(nickname: string): Promise<string | void> {
+    return fetch(`https://api.github.com/users/${nickname}`, {
+      method: "GET",
+    })
+      .then((response: Response) => response.json())
+      .then((json) => {
+        if (json.message === "Not Found") {
+          throw new Error("The user doesn't exist");
+        }
+        return json.repos_url;
+      });
   }
 
-  static getRepos() {
+  static getRepos(url: string) {
     // You code goes here
+    return fetch(url, {
+      method: "GET",
+    }).then((response) => response.json());
   }
 
   static printRepos() {
     // You code goes here
   }
 
-  
-  static printRepository() {
+  static printRepository(repo: string) {
     // You code goes here
+    return fetch(repo, {
+      method: "GET",
+    }).then((response) => response.json());
   }
-  
-  static getProjectInformations() {
+
+  static getProjectInformations(url: string) {
     // You code goes here
+    return fetch(url, {
+      method: "GET",
+    }).then((response) => response.json());
   }
 }
