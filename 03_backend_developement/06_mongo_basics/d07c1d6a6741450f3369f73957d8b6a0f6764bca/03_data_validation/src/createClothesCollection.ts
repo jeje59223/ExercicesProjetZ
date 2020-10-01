def00fa6166e370a6d3b2602b4dedfb78b53693b
jeje59223 <jcnockaert@hotmail.fr>
2020-10-01 16:39:33 +0200

    exercice 3 mongoDB validé

--- 03_backend_developement/06_mongo_basics/d07c1d6a6741450f3369f73957d8b6a0f6764bca/03_data_validation/src/createClothesCollection.ts
+++ 03_backend_developement/06_mongo_basics/d07c1d6a6741450f3369f73957d8b6a0f6764bca/03_data_validation/src/createClothesCollection.ts
@@ -2,18 +2,113 @@ import * as mongo from "mongodb";
 
 export const sweaterProperties = {
   // write your sweater properties here
+  required: ["_id", "name", "color", "category", "size"],
+  additionalProperties: false,
+  properties: {
+    _id: { bsonType: "objectId" },
+    name: {
+      bsonType: "string",
+      maxLength: 30,
+      description: "must be a string and is required with max 30 characters",
+    },
+    color: {
+      bsonType: "string",
+      maxLength: 15,
+      description: "must be a string and is required with max 15 characters",
+    },
+    category: {
+      bsonType: "string",
+      enum: ["sweater"],
+    },
+    size: {
+      bsonType: "string",
+      enum: ["XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+    },
+  },
 };
 
 export const shoesProperties = {
   // write your shoes properties here
+  required: ["_id", "name", "color", "category", "size"],
+  additionalProperties: false,
+  properties: {
+    _id: { bsonType: "objectId" },
+    name: {
+      bsonType: "string",
+      maxLength: 30,
+      description: "must be a string and is required with max 30 characters",
+    },
+    color: {
+      bsonType: "string",
+      maxLength: 15,
+      description: "must be a string and is required with max 15 characters",
+    },
+    category: {
+      bsonType: "string",
+      enum: ["shoes"],
+    },
+    size: {
+      bsonType: "int",
+      minimum: 30,
+      maximum: 50,
+    },
+    material: {
+      bsonType: "string",
+      enum: ["leather", "textile", "synthetic"],
+    }
+  },
 };
 
 export const pantsProperties = {
   // write your pants properties here
+  required: ["_id", "name", "color", "category", "size"],
+  additionalProperties: false,
+  properties: {
+    _id: { bsonType: "objectId" },
+    name: {
+      bsonType: "string",
+      maxLength: 30,
+      description: "must be a string and is required with max 30 characters",
+    },
+    color: {
+      bsonType: "string",
+      maxLength: 15,
+      description: "must be a string and is required with max 15 characters",
+    },
+    category: {
+      bsonType: "string",
+      enum: ["pants"],
+    },
+    size: {
+      bsonType: "object",
+      additionalProperties: false,
+      required: ["width"],
+      properties: {
+        width: {
+          bsonType: "int",
+          minimum: 32,
+          maximum: 46,
+          description: "must be integer between 32 and 46",
+        },
+        cut: {
+          bsonType: "string",
+          enum: ["slim", "skinny", "regular", "straight"],
+          description: "must be string",
+        },
+      }
+    }
+  }
 };
 
 export const clothesValidator = {
   // write your validator here
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["_id", "name", "color", "category", "size"],
+      oneOf: [{ sweaterProperties }, { shoesProperties }, { pantsProperties }],
+    },
+  },
 };
 
 export function createClothesCollection(
--- 03_backend_developement/06_mongo_basics/d07c1d6a6741450f3369f73957d8b6a0f6764bca/03_data_validation/src/createMoviesCollection.ts
+++ 03_backend_developement/06_mongo_basics/d07c1d6a6741450f3369f73957d8b6a0f6764bca/03_data_validation/src/createMoviesCollection.ts
@@ -2,6 +2,49 @@ import * as mongo from "mongodb";
 
 export const moviesValidator = {
   // write your validator here
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      additionalProperties: false,
+      required: ["_id", "title", "genre", "year"],
+      properties: {
+        _id: { bsonType: "objectId" },
+        title: {
+          bsonType: "string",
+          description: "must be a string and is required",
+        },
+        genre: {
+          bsonType: "string",
+          enum: ["action", "comedy", "dramatic", "horror", "romance"],
+        },
+        year: {
+          bsonType: "int",
+          minimum: 1950,
+          maximum: 2020,
+          description: "must be integer between 1950 and 2020",
+        },
+        ratings: {
+          bsonType: "object",
+          additionalProperties: false,
+          required: ["pressRating", "spectatorsRating"],
+          properties: {
+            pressRating: {
+              bsonType: "int",
+              minimum: 1,
+              maximum: 5,
+              description: "must be integer between 1 and 5",
+            },
+            spectatorsRating: {
+              bsonType: "int",
+              minimum: 1,
+              maximum: 5,
+              description: "must be integer between 1 and 5",
+            },
+          }
+        }
+      }
+    }
+  }
 };
 
 export function createMoviesCollection(
